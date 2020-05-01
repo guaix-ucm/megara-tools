ac0c57e133c70c6317abb874161ea7570e704fc2
Sergio Pascual <sergiopr@fis.ucm.es>
2020-05-01 17:50:49 +0200

    Move metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,50 +1,6 @@
 
-from setuptools import find_packages
-from setuptools import setup
+import pkg_resources
+import setuptools
 
-
-setup(
-    name='megaratools',
-    version='0.1.dev0',
-    author='Armando Gil de Paz',
-    author_email='agil@fis.ucm.es',
-    maintainer='Sergio Pascual',
-    maintainer_email='sergiopr@fis.ucm.es',
-    url='https://github.com/guaix-ucm/megarad-tools',
-    license='GPLv3',
-    description='Analysis and visualization of data from MEGARA instrument',
-    packages=find_packages(),
-    install_requires=[
-        'setuptools>=36.2.1',
-        'numpy',
-        'astropy',
-        'scipy',
-        'matplotlib',
-        'lmfit',
-        'numina',
-        'megaradrp',
-    ],
-    entry_points={
-        'console_scripts': [
-          'megaratools-analyze_rss = megaratools.analyze_rss:main',
-          'megaratools-analyze_spectrum = megaratools.analyze_spectrum:main',
-          'megaratools-diffuse_light = megaratools.diffuse_light:main',
-          'megaratools-extract_spectrum = megaratools.extract_spectrum:main',
-          'megaratools-plot_spectrum = megaratools.plot_spectrum:main',
-          'megaratools-rss_arith = megaratools.rss_arith:main',
-        ],
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        'Development Status :: 3 - Alpha',
-        "Environment :: Console",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Astronomy",
-    ],
-    long_description=open('README.md').read()
-)
+pkg_resources.require('setuptools>=39.2')
+setuptools.setup()
